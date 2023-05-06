2f99d4ca3f4c505263ab872d624f9ce6f4915c20
liu.ziwen <liu.ziwen@cn.fujitsu.com>
2023-05-06 14:36:08 +0900

    feat: wait to study Pipes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { CatsController } from "./cats.controller";
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+// import { AppController } from "./app.controller";
+// import { AppService } from "./app.service";
+import { CatsController, CatsModule } from "./cats";
+import { logger } from "./middleware";
+import { APP_FILTER } from "@nestjs/core";
+import { HttpExceptionFilter } from "./filter";
 
 @Module({
-  imports: [],
-  controllers: [AppController, CatsController],
-  providers: [AppService],
+  imports: [CatsModule],
+  // controllers: [AppController],
+  // providers: [AppService],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
-export class AppModule {}
+class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(logger).forRoutes(CatsController);
+  }
+}
+
+export { AppModule };
