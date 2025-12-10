76e5fb88aea7e22d9230d680efa5b08c9ebad5ba
RipperZero <1018744707@qq.com>
2025-12-11 05:21:48 +0800

    feat: ban faker

--- src/modules/graphql/authors/authors.service.ts
+++ src/modules/graphql/authors/authors.service.ts
@@ -1,15 +1,17 @@
 import { Author } from "@authors";
 import { Injectable } from "@nestjs/common";
 
-import { fakerJA as faker } from "@faker-js/faker";
+// import { fakerJA as faker } from "@faker-js/faker";
 
 @Injectable()
 class AuthorsService {
   findOneById(id: number) {
     const author: Author = {
       id: id,
-      firstName: faker.person.firstName(),
-      lastName: faker.person.lastName(),
+      // firstName: faker.person.firstName(),
+      // lastName: faker.person.lastName(),
+      firstName: "Zero",
+      lastName: "Ripper",
     };
 
     return author;
